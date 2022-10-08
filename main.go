4339876c17b79b9ab73f5cad24d2bf5d6baa6aa6
Matthew Coburn <mattcoburn24@gmail.com>
2022-10-07 17:58:58 -0700

    set up html and main func

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", handleForm)
+	http.HandleFunc("/hello", handleHello)
+
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil)
+	err != nil {
+		log.Fatal(err)
+	}
+}
+
